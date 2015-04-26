13cb91a5e06e88dfb00aa89bf284e904e83a1dd0
smobleyii <smobleyii@gmail.com>
2015-04-26 15:17:04 -0400

    update student match and animation

--- client/templates/includes/errors.js
+++ client/templates/includes/errors.js
@@ -3,3 +3,10 @@ Template.errors.helpers({
     return Errors.find();
   }
 });
+
+Template.errors.onRendered(function() {
+  var error = this.data;
+  Meteor.setTimeout(function() {
+    Errors.remove(error._id);
+  }, 3000);
+});

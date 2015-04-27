5fc52403e581bb9dcb5f141b1d3cc080e55d5116
smobleyii <smobleyii@gmail.com>
2015-04-26 21:43:08 -0400

    form validation

--- client/templates/students/student_submit.js
+++ client/templates/students/student_submit.js
@@ -1,3 +1,15 @@
+Template.studentSubmit.onCreated(function() {
+  Session.set('studentSubmitErrors', {});
+});
+Template.studentSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('studentSubmitErrors')[field];
+  },
+  errorClass: function(field) {
+    return !!Session.get('studentSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.studentSubmit.events({
   'submit form': function(e){
     e.preventDefault();
@@ -10,13 +22,17 @@ Template.studentSubmit.events({
       reg_packet: $(e.target).find('[name=reg_packet]').val()
     };
 
+    var errors = validateStudent(student);
+    if (errors.first_name || errors.last_name || errors.level || errors.dob)
+        return Session.set('studentSubmitErrors', errors);
+
     Meteor.call('studentInsert', student, function(error, result) {
       if (error)
         return throwError(error.reason);
 
       if (result.studentExists)
         throwError('This Student Already Exists');
-      
+
       Router.go('studentPage', {_id: result._id});
     });
   }

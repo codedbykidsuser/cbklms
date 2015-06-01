622906798943dd3f0244119a01cfef6cbb7a5340
smobleyii <smobleyii@gmail.com>
2015-06-01 06:54:38 -0400

    updated router, fixtures and publications for projects

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,10 +1,22 @@
 if (Students.find().count() === 0) {
-  Students.insert({
+  var now = new Date().getTime();
+
+  var sianiId = Students.insert({
     first_name: 'Siani',
     last_name: 'Mobley',
     level: 0,
     dob: '9/11/2005',
-    reg_packet: 'Yes'
+    reg_packet: 'Yes',
+    registered: new Date(now - 7 * 3600 * 1000)    
+  });
+
+  Projects.insert({
+    studentId: sianiId,
+    name: 'My Fairy Website',
+    description: 'I am building a basic html and css website about faires',
+    duration: '4',
+    lead: 'Sylvester Mobley',
+    objective: 'To learn how to add images'
   });
 
   Students.insert({

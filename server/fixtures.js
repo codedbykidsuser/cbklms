65f314ff8c5c108a39e56a4822a34b4561e15c22
smobleyii <smobleyii@gmail.com>
2015-06-01 23:14:43 -0400

    added initial project tracking

--- server/fixtures.js
+++ server/fixtures.js
@@ -26,4 +26,22 @@ if (Students.find().count() === 0) {
     dob: '7/13/1984',
     reg_packet: 'No'
   });
+
+  var jasonId = Students.insert({
+    first_name: 'Jason',
+    last_name: 'Nunn',
+    level: 3,
+    dob: '12/25/1984',
+    reg_packet: 'No',
+    registered: new Date(now - 7 * 3600 * 1000)    
+  });
+
+  Projects.insert({
+    studentId: jasonId,
+    name: 'Super Website',
+    description: 'This is the best website ever',
+    duration: '2',
+    lead: 'Sylvester Mobley',
+    objective: 'To learn how to add links'
+  });
 }
